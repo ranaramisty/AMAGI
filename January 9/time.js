3cded4b14ac45060e6da8d611ff4db6fe28453d2
ranaramisty <mistyclairer@yahoo.com>
2019-01-11 10:26:55 +0800

    Update

---
+++ January 9/time.js
@@ -0,0 +1,7 @@
+// import moment from "moment";
+
+// moment().add('days', 3).fromNow(); // "in 3 days"
+// moment().subtract('days', 4).fromNow(); // "4 days ago"
+// moment('October 2010').fromNow(); // "3 years ago"
+
+alert(`hey`);
